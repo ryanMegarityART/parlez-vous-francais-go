1e539c90c8471e77fe6a7c3ef9f8626c1a1b6075
ryanMegarityART <ryan.megarity@pshealthgroup.com>
2023-05-05 09:17:15 +0100

    add env for Port

--- go.mod
+++ go.mod
@@ -1,4 +1,3 @@
 module github.com/ryanMegarityART/parlez-vous-francais-go
 
 go 1.18
-
--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT not set")
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World!"))
 	})
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(port, nil)
 }
